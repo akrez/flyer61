258244c4a3093a07d841530b22da3f56697da6bd
Akrez <akrez.like@gmail.com>
2022-08-03 01:40:34 +0430

    feat:add admin seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        \App\Models\User::factory()->create([
+            'name' => 'ropco',
+            'email' => 'info@ropco.ir',
+            'email_verified_at' => null,
+            'password' => '$2y$10$6781/mW3MLmCRilyJI3PueGjiTYtwBAD5YJvllRleWcvy4f5hBgSO', //Admin1234
+            'remember_token' => '6jR67t3F5AmLjlpryTQHjssICjjH7frOamYaH72sXnSbwsR7PTHgfjoRlDp2',
+        ]);
+
         // \App\Models\User::factory(10)->create();
     }
 }
