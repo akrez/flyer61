f580670513c0ad0524cbac5bfe8f967b7c763aee
Akrez <akrez.like@gmail.com>
2022-08-12 19:44:32 +0430

    feat:complete entitylog

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EntityController;
+use App\Http\Controllers\EntityLogController;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\Route;
 
@@ -27,3 +28,5 @@ Route::get('/entity/upload', [EntityController::class, 'upload'])->name('entity-
 Route::post('/entity/import', [EntityController::class, 'import'])->name('entity-import');
 Route::get('/entity/export', [EntityController::class, 'export'])->name('entity-export');
 Route::get('/entity/index', [EntityController::class, 'index'])->name('entity-index');
+Route::get('/entitylog/export', [EntityLogController::class, 'export'])->name('entitylog-export');
+Route::get('/entitylog/index', [EntityLogController::class, 'index'])->name('entitylog-index');
